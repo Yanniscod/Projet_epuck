54ace4e3a1cc21c4e9727149a76c2fc43ed69d07
marguere <patrick.marguerettaz@epfl.ch>
2022-04-28 11:28:55 +0200

    get number lines done

--- Dos_projet/Projet/process_image.c
+++ Dos_projet/Projet/process_image.c
@@ -9,11 +9,48 @@
 #include <process_image.h>
 
 
-static float distance_cm = 0;
+//static float distance_cm = 0;
 
 //semaphore
 static BSEMAPHORE_DECL(image_ready_sem, TRUE);
 
+uint16_t get_nbr_lines(uint8_t *buffer){
+
+	uint32_t mean = 0;
+	uint16_t nbr_line=0;
+	uint16_t begin=0, end=0;
+	//performs an average
+	for(uint16_t i = 0 ; i < IMAGE_BUFFER_SIZE ; i++){
+		mean += buffer[i];
+	}
+	mean /= IMAGE_BUFFER_SIZE;
+
+	for (uint16_t i=0; i< IMAGE_BUFFER_SIZE;i++){
+
+		if(buffer[i]>mean && buffer[i+WIDTH_SLOPE]<mean)
+		{
+			begin=i;
+		}
+		//search for end
+		if(i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE) && begin!=0)
+		{
+
+			if(buffer[i] < mean && buffer[i+WIDTH_SLOPE] > mean)
+		    {
+				end=i;
+				if((end-begin)>MIN_LINE_WIDTH)
+				{
+				nbr_line=nbr_line+1;
+				begin=0;
+				end=0;
+				}
+			}
+
+		}
+	}		
+
+	return nbr_line;
+}
 static THD_WORKING_AREA(waCaptureImage, 256);
 static THD_FUNCTION(CaptureImage, arg) {
 
@@ -35,43 +72,7 @@ static THD_FUNCTION(CaptureImage, arg) {
 		chBSemSignal(&image_ready_sem);
     }
 }
-uint16_t get_nbr_lines(uint8_t *buffer){
 
-	uint32_t mean = 0;
-	uint16_t nbr_line=0;
-	uint16_t begin=0, end=0;
-	//performs an average
-	for(uint16_t i = 0 ; i < IMAGE_BUFFER_SIZE ; i++){
-		mean += buffer[i];
-	}
-	mean /= IMAGE_BUFFER_SIZE;
-
-	for (uint16_t i=0; i< IMAGE_BUFFER_SIZE;i++){
-
-		if(buffer[i]>mean && buffer[i+width_SLOPE]<mean)
-		{
-			begin=i;
-		}
-		//search for end
-		if(i < (IMAGE_BUFFER_SIZE - WIDTH_SLOPE) && begin!=0)
-		{
-
-			if(buffer[i] > mean && buffer[i+WIDTH_SLOPE] < mean)
-		    {
-				end=i
-				if((end-begin)<MIN_LINE_WIDTH)
-				{
-				nbr_line=nbr_line+1;
-				begin=0;
-				end=0;
-				}
-			}
-
-		}
-	}		
-
-	return nbr_line;
-}
 
 static THD_WORKING_AREA(waProcessImage, 1024);
 static THD_FUNCTION(ProcessImage, arg) {
@@ -82,6 +83,8 @@ static THD_FUNCTION(ProcessImage, arg) {
 	uint8_t *img_buff_ptr;
 	uint8_t image[IMAGE_BUFFER_SIZE] = {0};
 	uint16_t nbr_lines=0;
+	bool send_to_computer = true;
+
     while(1){
     	//waits until an image has been captured
         chBSemWait(&image_ready_sem);
@@ -96,11 +99,21 @@ static THD_FUNCTION(ProcessImage, arg) {
 			//extracts first 5bits of the first byte
 			//takes nothing from the second byte
 			image[i/2] = (uint8_t)img_buff_ptr[i]&0xF8;
-
-		// now need to find out what image is by determining how many lines
-			nbr_lines= get_nbr_lines(image)
-		
 		}
+		// now need to find out what image is by determining how many lines
+			nbr_lines= get_nbr_lines(image);
+
+
+			chprintf((BaseSequentialStream *)&SD3,"nombre lignes = %-7d\r\n",nbr_lines);
+			nbr_lines=0;
+			chThdSleepMilliseconds(1000);
+			if(send_to_computer)
+			{
+			//sends to the computer the image
+			SendUint8ToComputer(image, IMAGE_BUFFER_SIZE);
+			}
+			//invert the bool
+			send_to_computer = !send_to_computer;		
 
     }
 }
