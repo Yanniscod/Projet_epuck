768d532b2e886e4cec07406455cf257bf0766ea9
Yanniscod <yannis09@yahoo.fr>
2022-05-05 09:12:44 +0200

    Correction

--- Dos_projet/Projet/move.h
+++ Dos_projet/Projet/move.h
@@ -6,5 +6,5 @@ void rotate(int8_t nbr_90_turns);
 void set_nbr_rota(int8_t nbr_90_turns);
 void set_speed_rota(int16_t speed);
 void set_bool(int8_t bool_num, bool type);
-
+void take_puck(void);
 #endif /* MOVE_H_ */
