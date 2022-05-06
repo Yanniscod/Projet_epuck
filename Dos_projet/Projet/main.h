66a165f97e29769a659d71050acf16fa1d5ad119
Yanniscod <yannis09@yahoo.fr>
2022-05-06 16:37:08 +0200

    Corrections

--- Dos_projet/Projet/main.h
+++ Dos_projet/Projet/main.h
@@ -36,6 +36,7 @@ extern "C" {
 #define ZERO_SPEED 0 //[step/s]
 #define BASE_MOTOR_SPEED 350 //[steps/s]
 #define ONE_TURN_90_DEG 1
+#define DISTANCE_PUCK 1200 // valeur en steps à changer (1000 steps = 13cm)
 // steps values set experimentally
 #define STEPS_TURN 319 // number of steps needed for a 90 degrees turn
 #define STEPS_THRESHOLD 15 //[steps]
@@ -44,21 +45,12 @@ extern "C" {
 #define RESET_STEPS 1
 #define ROTATION 2
 #define MOVE_TO_PUCK 3
+#define GO_TO_GOAL 4
 
 //process_image.c
 #define IMAGE_BUFFER_SIZE	640
 #define MIN_LINE_WIDTH 30
 #define WIDTH_SLOPE 5
-
-#define DISTANCE_PUCK 1200 // valeur en steps à changer (1280=environ 16 cm)
-#define RESET_STEPS 1
-#define ROTATION 2
-#define MOVE_TO_PUCK 3
-#define GO_TO_GOAL 4
-
-#define GO 0
-#define FORWARD 1
-#define ROTA_TYPE 2
 /** Robot wide IPC bus. */
 extern messagebus_t bus;
 
